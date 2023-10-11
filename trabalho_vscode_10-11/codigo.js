c7f83a2fd4df191ef746832a66e51bd70bb16c34
Marc <127538568+mdubugras@users.noreply.github.com>
2023-10-11 15:03:04 -0300

    Add files via upload

---
+++ trabalho_vscode_10-11/codigo.js
@@ -0,0 +1,18 @@
+const ingrid = atletas[0]
+
+window.onload = () => {
+
+const espaco_nome = document.getElementById('nome');
+const espaco_imagem = document.querySelector('div#container > img');
+const espaco_descricao = document.querySelector('div#container > p');
+
+const preenche = (atleta) => {
+    espaco_nome.innerText = atleta.nome;
+    espaco_imagem.src = atleta.imagem
+    espaco_descricao.innerHTML = atleta.descricao;
+}
+
+preenche(atletas[1]);
+
+
+}
